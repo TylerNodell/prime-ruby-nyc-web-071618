fdaf709eed3e21779fb888e06f37071b3b2a7b10
Tyler Nodell <tyler.l.nodell@gmail.com>
2018-07-09 17:51:48 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,6 +1,9 @@
 # Add  code here!
 def prime?(n)
-  if (2..n-1).to_a.all? do |factor|
+  if num < 0 || num == 0 || num == 1
+    return false
+  else
+    (2..n-1).to_a.all? do |factor|
       num % factor != 0
     end
   end
